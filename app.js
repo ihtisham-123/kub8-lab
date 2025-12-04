5422eb8a71c9fb9e883b382bbbd7c2fbe888feec
ihtisham-12 <baberihtasham640@gmail.com>
2025-12-04 14:09:54 +0500

    cahlage 5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello from Kubernetes!");
+res.send("Hello v2!");
 });
 
 app.listen(8080, () => console.log("App running on 8080"));
